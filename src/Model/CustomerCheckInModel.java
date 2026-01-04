9fb58774633a375dd84c87a02614a62261521b87
Thineth <thinethgeewinda660@gmail.com>
2026-01-04 20:53:07 +0530

    Updated CustomerCheckInController.java and CustomerCheckInModel.java

--- src/Model/CustomerCheckInModel.java
+++ src/Model/CustomerCheckInModel.java
@@ -5,7 +5,6 @@ import MySqlConnection.RegistrationConnection;
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerCheckInModel {
