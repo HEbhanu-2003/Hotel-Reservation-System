b9fdb3740c57f7d31dc099dbaafe8a1eba9955fd
HEbhanu-2003 <sachiruushan@gmail.com>
2026-01-05 00:44:43 +0530

    Manage Rooms Data Base View Add in Customer CheckOut

---
+++ src/Model/CustomerCheckOutModel.java
@@ -0,0 +1,15 @@
+package Model;
+
+import MySqlConnection.RegistrationConnection;
+import View.CustomerCheckOutView;
+
+import javax.swing.table.DefaultTableModel;
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
+import java.util.Calendar;
+
+public class CustomerCheckOutModel {
+}
--- src/View/CustomerCheckOutView.java
+++ src/View/CustomerCheckOutView.java
@@ -1,7 +1,16 @@
 package View;
 
+import Controller.CustomerCheckOutController;
+import Model.CustomerCheckOutModel;
+import MySqlConnection.RegistrationConnection;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CustomerCheckOutView extends JFrame {
     private JPanel panel1;
@@ -21,5 +30,41 @@ public class CustomerCheckOutView extends JFrame {
         Container contentPane = this.getContentPane();
         contentPane.add(panel1);
 
+        MangeRoomsLoadTableData();
+
     }
+
+    private void MangeRoomsLoadTableData() {
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Room Number");
+        model.addColumn("Room Type");
+        model.addColumn("Bed Type");
+        model.addColumn("Days");
+        model.addColumn("Price");
+
+        try {
+            Connection con = RegistrationConnection.getDatabaseConnection();
+            String sql = "SELECT * FROM managerooms";
+            PreparedStatement pst = con.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                model.addRow(new Object[]{
+                        rs.getInt("RoomNumber"),
+                        rs.getString("RoomType"),
+                        rs.getString("Bed"),
+                        rs.getInt("Days"),
+                        rs.getDouble("Price")
+                });
+            }
+
+            table1.setModel(model);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
